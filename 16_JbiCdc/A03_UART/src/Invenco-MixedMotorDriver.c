f5a811f43ae966064c551accf5894eadff39e793
Mike <mike.qx.zhou@gmail.com>
2018-09-04 00:12:50 +1200

    Finished locator hub driver and bdcm driver

--- 16_JbiCdc/A03_UART/src/Invenco-MixedMotorDriver.c
+++ 16_JbiCdc/A03_UART/src/Invenco-MixedMotorDriver.c
@@ -69,11 +69,12 @@ struct MMD_command_state
 	enum MMD_command_e command;
 	
 	struct MMD_STEPPER_DATA steppersData[MMD_STEPPERS_AMOUNT];
-} mmd_command_state;
+} mmdCommand;
 
 enum MMD_BDCM_STATE
 {
-	BDCM_COAST = 0,
+	BDCM_ERROR = 0,
+	BDCM_COAST,
 	BDCM_REVERSE,
 	BDCM_FORWARD,
 	BDCM_BREAK
@@ -209,22 +210,360 @@ static bool MMD_clock_stepper(unsigned char stepperIndex, unsigned short steps)
 // power on all bi-direction direct current motor
 static void MMD_power_on_bdcms(bool on)
 {
-	
+	if(on) {
+		//activate PF7
+		PORTF_OUTSET = 0x80;
+		PORTF_DIRSET = 0x80;
+	}
+	else {
+		PORTF_DIRCLR = 0x80;
+	}
 }
 
 static bool MMD_are_bdcms_powered_on()
 {
-	
+	//read PF6
+	return (PORTF_IN & 0x40) != 0;
 }
 
-static void MMD_set_bdcm_state(unsigned char stepperIndex, enum MMD_BDCM_STATE state)
+static void MMD_set_bdcm_state(unsigned char index, enum MMD_BDCM_STATE state)
 {
-	
+	switch(index)
+	{
+		case 0: {
+			//bdcm1
+			switch(state)
+			{
+				case BDCM_COAST:
+				{
+					//IN1: 0; IN2: 0
+					PORTF_OUTCLR = 0x30;
+					PORTF_DIRSET = 0x30;
+				}
+				break;
+			
+				case BDCM_REVERSE:
+				{
+					//IN1: 0; IN2: 1
+					PORTF_OUTCLR = 0x20; 
+					PORTF_OUTSET = 0x10;
+					PORTF_DIRSET = 0x30;
+				}
+				break;
+			
+				case BDCM_FORWARD:
+				{
+					//IN1: 1; IN2: 0
+					PORTF_OUTSET = 0x20;
+					PORTF_OUTCLR = 0x10;
+					PORTF_DIRSET = 0x30;
+				}
+				break;
+			
+				case BDCM_BREAK:
+				{
+					//IN1: 1; IN2: 1
+					PORTF_OUTSET = 0x30;
+					PORTF_DIRSET = 0x30;
+				}
+				break;
+			
+				default:
+					break;
+			}
+		}
+		break;
+
+		case 1: {
+			//bdcm2
+			switch(state)
+			{
+				case BDCM_COAST:
+				{
+					//IN1: 0; IN2: 0
+					PORTF_OUTCLR = 0xC0;
+					PORTF_DIRSET = 0xC0;
+				}
+				break;
+				
+				case BDCM_REVERSE:
+				{
+					//IN1: 0; IN2: 1
+					PORTF_OUTCLR = 0x80;
+					PORTF_OUTSET = 0x40;
+					PORTF_DIRSET = 0xC0;
+				}
+				break;
+				
+				case BDCM_FORWARD:
+				{
+					//IN1: 1; IN2: 0
+					PORTF_OUTSET = 0x80;
+					PORTF_OUTCLR = 0x40;
+					PORTF_DIRSET = 0xC0;
+				}
+				break;
+				
+				case BDCM_BREAK:
+				{
+					//IN1: 1; IN2: 1
+					PORTF_OUTSET = 0xC0;
+					PORTF_DIRSET = 0xC0;
+				}
+				break;
+				
+				default:
+				break;
+			}
+		}
+		break;		
+
+		case 2: {
+			//bdcm3
+			switch(state)
+			{
+				case BDCM_COAST:
+				{
+					//IN1: 0; IN2: 0
+					PORTF_OUTCLR = 0x03;
+					PORTF_DIRSET = 0x03;
+				}
+				break;
+				
+				case BDCM_REVERSE:
+				{
+					//IN1: 0; IN2: 1
+					PORTF_OUTCLR = 0x02;
+					PORTF_OUTSET = 0x01;
+					PORTF_DIRSET = 0x03;
+				}
+				break;
+				
+				case BDCM_FORWARD:
+				{
+					//IN1: 1; IN2: 0
+					PORTF_OUTSET = 0x02;
+					PORTF_OUTCLR = 0x01;
+					PORTF_DIRSET = 0x03;
+				}
+				break;
+				
+				case BDCM_BREAK:
+				{
+					//IN1: 1; IN2: 1
+					PORTF_OUTSET = 0x03;
+					PORTF_DIRSET = 0x03;
+				}
+				break;
+				
+				default:
+				break;
+			}
+		}
+		break;	
+
+		case 3: {
+			//bdcm4
+			switch(state)
+			{
+				case BDCM_COAST:
+				{
+					//IN1: 0; IN2: 0
+					PORTE_OUTCLR = 0x30;
+					PORTE_DIRSET = 0x30;
+				}
+				break;
+				
+				case BDCM_REVERSE:
+				{
+					//IN1: 0; IN2: 1
+					PORTE_OUTCLR = 0x20;
+					PORTE_OUTSET = 0x10;
+					PORTE_DIRSET = 0x30;
+				}
+				break;
+				
+				case BDCM_FORWARD:
+				{
+					//IN1: 1; IN2: 0
+					PORTE_OUTSET = 0x20;
+					PORTE_OUTCLR = 0x10;
+					PORTE_DIRSET = 0x30;
+				}
+				break;
+				
+				case BDCM_BREAK:
+				{
+					//IN1: 1; IN2: 1
+					PORTE_OUTSET = 0x30;
+					PORTE_DIRSET = 0x30;
+				}
+				break;
+				
+				default:
+				break;
+			}
+		}
+		break;
+
+		case 4: {
+			//bdcm5
+			switch(state)
+			{
+				case BDCM_COAST:
+				{
+					//IN1: 0; IN2: 0
+					PORTE_OUTCLR = 0xC0;
+					PORTE_DIRSET = 0xC0;
+				}
+				break;
+				
+				case BDCM_REVERSE:
+				{
+					//IN1: 0; IN2: 1
+					PORTE_OUTCLR = 0x80;
+					PORTE_OUTSET = 0x40;
+					PORTE_DIRSET = 0xC0;
+				}
+				break;
+				
+				case BDCM_FORWARD:
+				{
+					//IN1: 1; IN2: 0
+					PORTE_OUTSET = 0x80;
+					PORTE_OUTCLR = 0x40;
+					PORTE_DIRSET = 0xC0;
+				}
+				break;
+				
+				case BDCM_BREAK:
+				{
+					//IN1: 1; IN2: 1
+					PORTE_OUTSET = 0xC0;
+					PORTE_DIRSET = 0xC0;
+				}
+				break;
+				
+				default:
+				break;
+			}
+		}
+		break;
+
+		case 5: {
+			//bdcm6
+			switch(state)
+			{
+				case BDCM_COAST:
+				{
+					//IN1: 0; IN2: 0
+					PORTE_OUTCLR = 0x03;
+					PORTE_DIRSET = 0x03;
+				}
+				break;
+				
+				case BDCM_REVERSE:
+				{
+					//IN1: 0; IN2: 1
+					PORTE_OUTCLR = 0x02;
+					PORTE_OUTSET = 0x01;
+					PORTE_DIRSET = 0x03;
+				}
+				break;
+				
+				case BDCM_FORWARD:
+				{
+					//IN1: 1; IN2: 0
+					PORTE_OUTSET = 0x02;
+					PORTE_OUTCLR = 0x01;
+					PORTE_DIRSET = 0x03;
+				}
+				break;
+				
+				case BDCM_BREAK:
+				{
+					//IN1: 1; IN2: 1
+					PORTE_OUTSET = 0x03;
+					PORTE_DIRSET = 0x03;
+				}
+				break;
+				
+				default:
+				break;
+			}
+		}
+		break;			
+		
+		default:
+			break;
+	}
 }
 
-static enum MMD_BDCM_STATE MMD_get_bdcm_state(unsigned char stepperIndex)
+static enum MMD_BDCM_STATE MMD_get_bdcm_state(unsigned char index)
 {
+	unsigned char data = 0;
 	
+	switch(index)
+	{
+		case 0: //bdcm1
+		{
+			data = (PORTF_IN >> 4) & 0x03;
+		}
+		break;
+		
+		case 1: //bdcm2
+		{
+			data = (PORTF_IN >> 2) & 0x03;
+		}
+		break;
+		
+		case 2: //bdcm3
+		{
+			data = (PORTF_IN) & 0x03;
+		}
+		break;
+		
+		case 3: //bdcm4
+		{
+			data = (PORTE_IN >> 4) & 0x03;
+		}
+		break;
+
+		case 4: //bdcm5
+		{
+			data = (PORTE_IN >> 2) & 0x03;
+		}
+		break;
+		
+		case 5: //bdcm6
+		{
+			data = (PORTE_IN) & 0x03;
+		}
+		break;
+		
+		default:
+			return BDCM_ERROR;
+	}
+	
+	switch(data)
+	{
+		case 0:
+		{
+			return BDCM_COAST;
+		}
+		case 1:
+		{
+			return BDCM_REVERSE;
+		}
+		case 2:
+		{
+			return BDCM_FORWARD;
+		}
+		default:
+		{
+			return BDCM_BREAK;
+		}
+	}
 }
 
 
@@ -235,17 +574,209 @@ static enum MMD_BDCM_STATE MMD_get_bdcm_state(unsigned char stepperIndex)
 //	others: error occurs.
 static unsigned char MMD_locator_get(unsigned char locatorIndex)
 {
-	
+	switch(locatorIndex)
+	{
+		case 0: //LC1
+			if((PORTD_IN & 0x10) == 0)
+				return 1;
+			else if((PORTD_IN & 0x20) == 0)
+				return 2;
+			else 
+				return 0;
+		
+		case 1: //LC2
+			if((PORTC_IN & 0x80) != 0)
+				return 0;
+			else {
+				unsigned char n = 0;
+				
+				if((PORTC_IN & 0x40) != 0)
+					n += 1;
+				if((PORTC_IN & 0x20) != 0)
+					n += 2;
+				if((PORTC_IN & 0x10) != 0)
+					n += 4;
+				return n;
+			}
+		
+		case 2: //LC3
+			if((PORTC_IN & 0x08) != 0)
+				return 0;
+			else {
+				unsigned char n = 0;
+				
+				if((PORTC_IN & 0x04) != 0)
+					n += 1;
+				if((PORTC_IN & 0x02) != 0)
+					n += 2;
+				if((PORTC_IN & 0x01) != 0)
+					n += 4;
+				return n;
+			}
+		
+		case 3: //LC4
+			if((PORTB_IN & 0x80) != 0)
+				return 0;
+			else {
+				unsigned char n = 0;
+			
+				if((PORTB_IN & 0x40) != 0)
+					n += 1;
+				if((PORTB_IN & 0x20) != 0)
+					n += 2;
+				if((PORTB_IN & 0x10) != 0)
+					n += 4;
+				return n;
+			}
+		
+		case 4: //LC5
+			if((PORTB_IN & 0x08) != 0)
+				return 0;
+			else {
+				unsigned char n = 0;
+			
+				if((PORTB_IN & 0x04) != 0)
+					n += 1;
+				if((PORTB_IN & 0x02) != 0)
+					n += 2;
+				if((PORTB_IN & 0x01) != 0)
+					n += 4;
+				return n;
+			}
+				
+		case 5: //LC6
+			if((PORTA_IN & 0x80) != 0)
+				return 0;
+			else {
+				unsigned char n = 0;
+				
+				if((PORTA_IN & 0x40) != 0)
+					n += 1;
+				if((PORTA_IN & 0x20) != 0)
+					n += 2;
+				if((PORTA_IN & 0x10) != 0)
+					n += 4;
+				return n;
+			}
+		
+		case 6: //LC7
+			if((PORTA_IN & 0x08) != 0)
+				return 0;
+			else {
+				unsigned char n = 0;
+				
+				if((PORTA_IN & 0x04) != 0)
+					n += 1;
+				if((PORTA_IN & 0x02) != 0)
+					n += 2;
+				if((PORTA_IN & 0x01) != 0)
+					n += 4;
+				return n;
+			}
+		
+		case 7: //LC8
+			if((PORTR_IN & 0x02) != 0)
+				return 0;
+			else {
+				unsigned char n = 0;
+				
+				if((PORTR_IN & 0x01) != 0)
+					n += 1;
+				if((PORTQ_IN & 0x08) != 0)
+					n += 2;
+				if((PORTQ_IN & 0x04) != 0)
+					n += 4;
+				return n;
+			}
+		
+		default:
+			return 0;
+	}
 }
 
-static void MMD_check_status()
+static void MMD_parse_command()
 {
-	
+	unsigned char cmd;
+	unsigned char param = 0;
+	bool validCmd = true;
+				
+	cmd = readInputBuffer();
+	printString("CMD:");printHex(cmd);printString("\r\n");
+	if(0 == cmd) {
+		validCmd = false;
+	}
+	else if(0x0D == cmd) {
+		validCmd = false;
+	}
+	else {
+		//read parameter of the command
+		for(;;) {
+			unsigned char c;
+			c = readInputBuffer();
+						
+			if((c >= '0') && (c <= '9')) {
+				param =  param * 10 + c - '0';
+			}
+			else if(0x0D == c) {
+				break; //end of a command
+			}
+			else {
+				//illegal character in parameter
+				writeOutputBufferString("Illegal parameters\r\n");
+				clearInputBuffer();
+				validCmd = false;
+				break;
+			}
+		}
+	}
+				
+	printString("param:");printHex(param);printString("\r\n");
+	if(!validCmd) {
+		;
+	}
+	else
+	{
+		switch(cmd)
+		{
+			case 'I':
+			case 'i':
+			// Insert smart card.
+			break;
+						
+			case 'P':
+			case 'p':
+			// Pullout smart card.
+			break;
+						
+			case 'C':
+			case 'c':
+			//connect smart card
+			break;
+						
+			case 'D':
+			case 'd':
+			//solenoid duration division
+			break;
+						
+			case 'Q':
+			case 'q':
+			// Query
+			break;
+			default:
+			writeOutputBufferString("Invalid command\r\n");
+			break;
+		}
+	}
 }
 
 static void MMD_run_command()
 {
 	
+	
+}
+
+static void MMD_check_status()
+{
 	
 }
 
@@ -262,7 +793,7 @@ void ecd300MixedMotorDrivers()
 	
 	while(1)
 	{
-		unsigned char key, tdo;
+		unsigned char key;
 		
 		if (udi_cdc_is_rx_ready())
 		{
@@ -275,9 +806,6 @@ void ecd300MixedMotorDrivers()
 				writeOutputBufferChar(0x0A); //append a line feed.
 			}
 			
-			printHex(key);
-			printString("\r\n");
-			
 			//toggle PD0 to indicate character reception.
 			if(PORTD_IN&0x01) {
 				PORTD_OUTCLR = 0x01;
@@ -287,79 +815,9 @@ void ecd300MixedMotorDrivers()
 			}
 			
 			// 0x0D is command terminator
-			if((key == 0x0D) && (mmd_command_state.state == AWAITING_COMMAND))
+			if((key == 0x0D) && (mmdCommand.state == AWAITING_COMMAND))
 			{
-				unsigned char cmd;
-				unsigned char param = 0;
-				bool validCmd = true;
-				
-				cmd = readInputBuffer();
-				printString("CMD:");printHex(cmd);printString("\r\n");
-				if(0 == cmd) {
-					validCmd = false;
-				}
-				else if(0x0D == cmd) {
-					validCmd = false;
-				}
-				else {
-					//read parameter of the command
-					for(;;) {
-						unsigned char c;
-						c = readInputBuffer();
-						
-						if((c >= '0') && (c <= '9')) {
-							param =  param * 10 + c - '0';
-						}
-						else if(0x0D == c) {
-							break; //end of a command
-						}
-						else {
-							//illegal character in parameter
-							writeOutputBufferString("Illegal parameters\r\n");
-							clearInputBuffer();
-							validCmd = false;
-							break;
-						}
-					}
-				}
-				
-				printString("param:");printHex(param);printString("\r\n");
-				if(!validCmd) {
-					;
-				}
-				else
-				{
-					switch(cmd)
-					{
-						case 'I':
-						case 'i':
-						// Insert smart card.
-						break;
-						
-						case 'P':
-						case 'p':
-						// Pullout smart card.
-						break;
-						
-						case 'C':
-						case 'c':
-						//connect smart card
-						break;
-						
-						case 'D':
-						case 'd':
-						//solenoid duration division
-						break;
-						
-						case 'Q':
-						case 'q':
-						// Query
-						break;
-						default:
-						writeOutputBufferString("Invalid command\r\n");
-						break;
-					}
-				}
+				MMD_parse_command();
 			}
 		}
 
