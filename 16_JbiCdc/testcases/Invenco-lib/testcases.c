f194c1cb3cec5f0be7e6b566a57c7953d5be8fe8
user1 <mikez@invenco.com>
2021-03-12 14:26:49 +1300

    test that multi app data can be sent

--- 16_JbiCdc/testcases/Invenco-lib/testcases.c
+++ 16_JbiCdc/testcases/Invenco-lib/testcases.c
@@ -1143,24 +1143,97 @@ static void _001002_uart_sendAppDataAround()
 	printf("-------------------------------------\r\n");
 }
 
+static void _001003_uart_multiAppData()
+{
+	printf("\r\n===================================\r\n");
+	printf("%s started\r\n", __FUNCTION__);
+
+	_resetTestEnv();
+
+	unsigned char buffer[64];
+	unsigned char monitorBuffer[64];
+	unsigned long crc;
+	int rc;
+	int len;
+	unsigned char packetId;
+	unsigned char expectedId = 0;
+
+	unsigned char * pChars = "hello world";
+
+	for(int loop=0; loop<0xffff; loop++)
+	{
+		memset(buffer, 0, 64);
+		memset(monitorBuffer, 0, 64);
+		enableOutputBuffer();
+		writeOutputBufferString(pChars);
+		pollScsDataExchange(); // output idle -> sending data
+		pollScsDataExchange(); // sending data -> waiting ack
+		rc = uartConsumeData(buffer, 64);
+		ASSERT(rc == (strlen(pChars) + SCS_DATA_PACKET_STAFF_LENGTH));
+		crc_set_initial_value(0xffff);
+		crc = crc_io_checksum(buffer, rc - 2, 0);
+		ASSERT((crc & 0xff) == buffer[rc - 2]);
+		ASSERT(((crc >> 8) & 0xff) == buffer[rc - 1]);
+		ASSERT(buffer[2] == strlen(pChars));
+		for(int i=0; i<strlen(pChars); i++) 
+		{
+			ASSERT(buffer[3+i] == pChars[i]);
+		}
+		packetId = buffer[1];
+		ASSERT(packetId == expectedId);
+		sprintf(monitorBuffer, "< D %02X\r\n", packetId);
+		memset(buffer, 0, 64);
+		rc = usbConsumeData(buffer, 64);
+		ASSERT(rc == strlen(monitorBuffer));
+		ASSERT(strcmp(monitorBuffer, buffer) == 0);
+		ASSERT(inputStageState() == SCS_INPUT_IDLE);
+		ASSERT(outputStageState() == SCS_OUTPUT_WAIT_ACK);
+
+		tcClocks(outputStageTimeoutValue() / 2);
+
+		rc = _createAckPacket(packetId, buffer, 64);
+		ASSERT(rc == 4);
+		uartProduceData(buffer, rc);
+		for(int i=0; i<rc; i++) {
+			pollScsDataExchange();
+		}
+		sprintf(buffer, "> A %02X\r\n", packetId);
+		rc = usbConsumeData(monitorBuffer, 64);
+		len = strlen(buffer);
+		ASSERT(rc == len);
+		ASSERT(strcmp(buffer, monitorBuffer) == 0);
+		ASSERT(inputStageState() == SCS_INPUT_IDLE);
+		ASSERT(outputStageState() == SCS_OUTPUT_IDLE);		
+
+		expectedId++;
+		if(expectedId == SCS_INVALID_PACKET_ID) {
+			expectedId = 1;
+		}	
+	}
+
+	printf("%s stopped\r\n", __FUNCTION__);
+	printf("-------------------------------------\r\n");
+}
+
 void startTestCases()
 {
 	printf("startTestCases started\r\n");
 
-	_002000_tc();
-	_000001_uart_oneByteInput();
-	_000002_uart_inputStageTimeout();
-	_000003_uart_completeAckPacket();
-	_000004_uart_oneCompleteDataPacket();
-	_000005_uart_repeatedCompleteDataPacket();
-	_000006_uart_duplicatedDataPacket();
-	_000007_uart_discontinuousPakcetId();
-	_000008_uart_crcError();
-	_000009_uart_illegalPacketLength();
-	_000010_uart_abruptPacketId();
-	_001000_uart_sendAppData();
-	_001001_uart_sendAppDataTimeout();
-	_001002_uart_sendAppDataAround();
+	// _002000_tc();
+	// _000001_uart_oneByteInput();
+	// _000002_uart_inputStageTimeout();
+	// _000003_uart_completeAckPacket();
+	// _000004_uart_oneCompleteDataPacket();
+	// _000005_uart_repeatedCompleteDataPacket();
+	// _000006_uart_duplicatedDataPacket();
+	// _000007_uart_discontinuousPakcetId();
+	// _000008_uart_crcError();
+	// _000009_uart_illegalPacketLength();
+	// _000010_uart_abruptPacketId();
+	// _001000_uart_sendAppData();
+	// _001001_uart_sendAppDataTimeout();
+	// _001002_uart_sendAppDataAround();
+	_001003_uart_multiAppData();
 
 	printf("startTestCases finished\r\n");
 }
